0cef3a7dcd7629d61fd6aa25420b38d488ff56d3
Sergey Kishenin <sergey.kishenin@gmail.com>
2014-10-13 23:14:30 +0600

    implement new fileuploader api

--- app/models/redactor_file.rb
+++ app/models/redactor_file.rb
@@ -1,3 +1,22 @@
 class RedactorFile < ActiveRecord::Base
+  attr_writer :name
+
   mount_uploader :file, RedactorFileUploader
+
+  def title
+    File.basename(file.path)
+  end
+  alias :name :title
+
+  def link
+    file.url
+  end
+
+  def size
+    file.size.to_filesize
+  end
+
+  def as_json options = {}
+    super(only: [], methods: [:title, :name, :link, :size])
+  end
 end
--- lib/imperavi_redactor_rails.rb
+++ lib/imperavi_redactor_rails.rb
@@ -1,2 +1,3 @@
 require "imperavi_redactor_rails/engine"
 require "imperavi_redactor_rails/carrierwave_string_io"
+require "imperavi_redactor_rails/fixnum"
